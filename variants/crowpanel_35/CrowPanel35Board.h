154fb075b18a8d88fd776bb9573497b9626136ca
docmeth02 <docmeth02@googlemail.com>
2026-01-19 17:58:05 +0100

    Fix hibernate/deep sleep on crowpanel 3.5"

--- variants/crowpanel_35/CrowPanel35Board.h
+++ variants/crowpanel_35/CrowPanel35Board.h
@@ -2,10 +2,13 @@
 
 #include <Arduino.h>
 #include <helpers/ESP32Board.h>
+#include <driver/rtc_io.h>
+#include <esp_sleep.h>
 
 // CrowPanel 3.5" Advance specific pins
 #define PIN_LORA_ENABLE     45   // Must be LOW to enable LoRa (disables microphone)
 #define PIN_DISPLAY_BL      38   // Display backlight
+#define PIN_TOUCH_RST       48   // Touch controller reset
 
 class CrowPanel35Board : public ESP32Board {
 public:
@@ -22,6 +25,24 @@ public:
 #endif
 
     ESP32Board::begin();
+  }
+
+  void powerOff() override {
+    // Turn off display backlight
+    pinMode(PIN_DISPLAY_BL, OUTPUT);
+    digitalWrite(PIN_DISPLAY_BL, LOW);
+    gpio_hold_en((gpio_num_t)PIN_DISPLAY_BL);
+
+    // Hold touch controller in reset to prevent spurious interrupts
+    pinMode(PIN_TOUCH_RST, OUTPUT);
+    digitalWrite(PIN_TOUCH_RST, LOW);
+    gpio_hold_en((gpio_num_t)PIN_TOUCH_RST);
+
+    // Disable ALL wake sources - only power cycle will wake device
+    esp_sleep_disable_wakeup_source(ESP_SLEEP_WAKEUP_ALL);
+
+    // Enter deep sleep - this never returns
+    esp_deep_sleep_start();
   }
 
   const char* getManufacturerName() const override {
